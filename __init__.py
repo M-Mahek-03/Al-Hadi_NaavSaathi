89cfe00f5d5e5365f88d37d5a1fd0ac209a9dee6
M-Mahek-03 <mnmukadam04@gmail.com>
2025-09-20 11:27:57 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
-"""WebSocket protocol versions 13 and 8."""
+from __future__ import annotations
+
+from .base import ANY
+from .base import default_namespace
+from .base import NamedSignal
+from .base import Namespace
+from .base import Signal
+from .base import signal
+
+__all__ = [
+    "ANY",
+    "default_namespace",
+    "NamedSignal",
+    "Namespace",
+    "Signal",
+    "signal",
+]
