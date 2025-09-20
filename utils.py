83702d8c49be23d6a94b365dde1ed2dcac66be19
M-Mahek-03 <mnmukadam04@gmail.com>
2025-09-20 11:34:21 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,97 +1,71 @@
-"""Utility functions for aiohappyeyeballs."""
+# -*- coding: utf-8 -*-
+"""
+This module offers general convenience and utility functions for dealing with
+datetimes.
 
-import ipaddress
-import socket
-from typing import Dict, List, Optional, Tuple, Union
+.. versionadded:: 2.7.0
+"""
+from __future__ import unicode_literals
 
-from .types import AddrInfoType
+from datetime import datetime, time
 
 
-def addr_to_addr_infos(
-    addr: Optional[
-        Union[Tuple[str, int, int, int], Tuple[str, int, int], Tuple[str, int]]
-    ],
-) -> Optional[List[AddrInfoType]]:
-    """Convert an address tuple to a list of addr_info tuples."""
-    if addr is None:
-        return None
-    host = addr[0]
-    port = addr[1]
-    is_ipv6 = ":" in host
-    if is_ipv6:
-        flowinfo = 0
-        scopeid = 0
-        addr_len = len(addr)
-        if addr_len >= 4:
-            scopeid = addr[3]  # type: ignore[misc]
-        if addr_len >= 3:
-            flowinfo = addr[2]  # type: ignore[misc]
-        addr = (host, port, flowinfo, scopeid)
-        family = socket.AF_INET6
+def today(tzinfo=None):
+    """
+    Returns a :py:class:`datetime` representing the current day at midnight
+
+    :param tzinfo:
+        The time zone to attach (also used to determine the current day).
+
+    :return:
+        A :py:class:`datetime.datetime` object representing the current day
+        at midnight.
+    """
+
+    dt = datetime.now(tzinfo)
+    return datetime.combine(dt.date(), time(0, tzinfo=tzinfo))
+
+
+def default_tzinfo(dt, tzinfo):
+    """
+    Sets the ``tzinfo`` parameter on naive datetimes only
+
+    This is useful for example when you are provided a datetime that may have
+    either an implicit or explicit time zone, such as when parsing a time zone
+    string.
+
+    .. doctest::
+
+        >>> from dateutil.tz import tzoffset
+        >>> from dateutil.parser import parse
+        >>> from dateutil.utils import default_tzinfo
+        >>> dflt_tz = tzoffset("EST", -18000)
+        >>> print(default_tzinfo(parse('2014-01-01 12:30 UTC'), dflt_tz))
+        2014-01-01 12:30:00+00:00
+        >>> print(default_tzinfo(parse('2014-01-01 12:30'), dflt_tz))
+        2014-01-01 12:30:00-05:00
+
+    :param dt:
+        The datetime on which to replace the time zone
+
+    :param tzinfo:
+        The :py:class:`datetime.tzinfo` subclass instance to assign to
+        ``dt`` if (and only if) it is naive.
+
+    :return:
+        Returns an aware :py:class:`datetime.datetime`.
+    """
+    if dt.tzinfo is not None:
+        return dt
     else:
-        addr = (host, port)
-        family = socket.AF_INET
-    return [(family, socket.SOCK_STREAM, socket.IPPROTO_TCP, "", addr)]
+        return dt.replace(tzinfo=tzinfo)
 
 
-def pop_addr_infos_interleave(
-    addr_infos: List[AddrInfoType], interleave: Optional[int] = None
-) -> None:
+def within_delta(dt1, dt2, delta):
     """
-    Pop addr_info from the list of addr_infos by family up to interleave times.
-
-    The interleave parameter is used to know how many addr_infos for
-    each family should be popped of the top of the list.
+    Useful for comparing two datetimes that may have a negligible difference
+    to be considered equal.
     """
-    seen: Dict[int, int] = {}
-    if interleave is None:
-        interleave = 1
-    to_remove: List[AddrInfoType] = []
-    for addr_info in addr_infos:
-        family = addr_info[0]
-        if family not in seen:
-            seen[family] = 0
-        if seen[family] < interleave:
-            to_remove.append(addr_info)
-        seen[family] += 1
-    for addr_info in to_remove:
-        addr_infos.remove(addr_info)
-
-
-def _addr_tuple_to_ip_address(
-    addr: Union[Tuple[str, int], Tuple[str, int, int, int]],
-) -> Union[
-    Tuple[ipaddress.IPv4Address, int], Tuple[ipaddress.IPv6Address, int, int, int]
-]:
-    """Convert an address tuple to an IPv4Address."""
-    return (ipaddress.ip_address(addr[0]), *addr[1:])
-
-
-def remove_addr_infos(
-    addr_infos: List[AddrInfoType],
-    addr: Union[Tuple[str, int], Tuple[str, int, int, int]],
-) -> None:
-    """
-    Remove an address from the list of addr_infos.
-
-    The addr value is typically the return value of
-    sock.getpeername().
-    """
-    bad_addrs_infos: List[AddrInfoType] = []
-    for addr_info in addr_infos:
-        if addr_info[-1] == addr:
-            bad_addrs_infos.append(addr_info)
-    if bad_addrs_infos:
-        for bad_addr_info in bad_addrs_infos:
-            addr_infos.remove(bad_addr_info)
-        return
-    # Slow path in case addr is formatted differently
-    match_addr = _addr_tuple_to_ip_address(addr)
-    for addr_info in addr_infos:
-        if match_addr == _addr_tuple_to_ip_address(addr_info[-1]):
-            bad_addrs_infos.append(addr_info)
-    if bad_addrs_infos:
-        for bad_addr_info in bad_addrs_infos:
-            addr_infos.remove(bad_addr_info)
-        return
-    raise ValueError(f"Address {addr} not found in addr_infos")
+    delta = abs(delta)
+    difference = dt1 - dt2
+    return -delta <= difference <= delta
